8ff90fdfb03fe333ecff5908e44ad16acef51ce5
brianxautumn <shred@brianautumn.com>
2016-06-27 20:36:03 -0700

    Started some basic setup to play sound

--- test/test.js
+++ test/test.js
@@ -1,5 +1,34 @@
-(function() {
-    var audioengine = require('../src/audioengine.js');
+(function () {
+    
+    var FlareAudioEngine = require('../src/audioengine.js'); // import the audio engine
+    var playButton = document.getElementById("playButton");
+    
+    initPlayer = function () {
+        playButton.disabled = false; //ready to play!
+    }
+
+    
+    var audioengine = new FlareAudioEngine(); // create new engine	
+    audioengine.setBootEvent(initPlayer);														// audio engine
+
+
+    playButton.disabled = true;
+
+    var sound;
+    var request = new XMLHttpRequest();
+    request.open('GET', "intro.m4a", true);
+    request.responseType = 'arraybuffer';
+
+    request.onload = function () {
+        audioengine.boot(this.response);
+    }
+
+
+
+    request.send();
+    
+    playButton.onclick = function(){
+        audioengine.play();
+    };
+
 })();
-
-
